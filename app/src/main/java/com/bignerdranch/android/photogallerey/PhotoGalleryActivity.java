28858b31173fb03544769cd1c91d0668b3231174
pumpkinchen <lingchen1019@163.com>
2017-04-11 15:05:10 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallerey/PhotoGalleryActivity.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.photogallerey;
+
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
